d679b5022282c7a62d938cff6f1094ab8d1db419
Matheus Sexto <matheus.sexto@gmail.com>
2023-02-05 20:06:26 +0100

    update key

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -10,21 +10,19 @@ from datetime import datetime
 # App Insights
 # TODO: Import required libraries for App Insights
 from opencensus.ext.azure.log_exporter import AzureLogHandler
-from opencensus.ext.azure.log_exporter import AzureEventHandler
 from opencensus.ext.azure import metrics_exporter
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 from opencensus.stats import aggregation as aggregation_module
 from opencensus.stats import measure as measure_module
 from opencensus.stats import stats as stats_module
 from opencensus.stats import view as view_module
 from opencensus.tags import tag_map as tag_map_module
-from opencensus.trace import config_integration
-from opencensus.ext.azure.trace_exporter import AzureExporter
 from opencensus.trace.samplers import ProbabilitySampler
 from opencensus.trace.tracer import Tracer
-from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 
 
-connection_string = "InstrumentationKey=228305a9-aa04-43ec-8392-474e39f890f0;IngestionEndpoint=https://uksouth-1.in.applicationinsights.azure.com/;LiveEndpoint=https://uksouth.livediagnostics.monitor.azure.com/"
+connection_string = 'InstrumentationKey=228305a9-aa04-43ec-8392-474e39f890f0'
 
 # Logging
 # TODO: Setup logger
